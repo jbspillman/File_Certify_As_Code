b3a17c35c293ca94bb5da0056281f889c217ecd4
Bryan Spillman <45645798+jbspillman@users.noreply.github.com>
2026-02-24 21:52:06 -0500

    Add files via upload

---
+++ NFS4_Test_Suite.py
@@ -0,0 +1,15 @@
+""" NFS4 Test Suite  """
+
+
+def test_nfs4_mount()
+def test_nfs4_file_read()
+def test_nfs4_file_write()
+def test_nfs4_file_delete()
+def test_nfs4_file_rename()
+def test_nfs4_delegation_granted()
+def test_nfs4_byte_range_locking()
+def test_nfs4_acl_access_allowed()
+def test_nfs4_acl_access_denied()
+def test_nfs4_large_file_checksum()
+def test_nfs4_export_access_allowed()
+def test_nfs4_export_access_denied()
